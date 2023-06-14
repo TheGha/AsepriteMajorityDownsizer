a28b85e14adac286b39dd4174ceaf9e0731a6953
TheGha <jonatanrehn@hotmail.com>
2023-06-14 23:07:25 +0200

    Update MajorityDownsize.lua
    Updated the code to fix some old bugs that i don't remember the details of, think it was some cases where the new cell or whatever was created outside of image boundaries, or of center.
    
    also added the functionality for alpha 0 pixels to have a multiplier.

--- MajorityDownsize.lua
+++ MajorityDownsize.lua
@@ -2,8 +2,9 @@ local spr = app.activeSprite
 if not spr then return app.alert "No active sprite" end
 if not spr.colorMode == ColorMode.RGB then return app.alert "Color mode must be RGB" end
 
-local dlg = Dialog("Majority downsize")
+local dlg = Dialog("Majority downsize weighted")
 dlg:entry{ id="downsize", label="Downsize", text="0", focus=true }
+   :entry{ id="invisibleFactor", label="InvisibleFactor", text="1"}
    :button{ id="ok", text="OK", focus=true }
    :button{ text="Cancel" }
 dlg:show()
@@ -19,26 +20,44 @@ if data.ok then
         
             local colors = {}
 
-            -- Loop through each 3x3 chunk of pixels
+            -- Loop through each (downsizeI x downsizeI) chunk of pixels
             for y = 0, imgInput.height - 1, downsizeI do
                 for x = 0, imgInput.width - 1, downsizeI do
                     -- Create a table to store the pixel counts
                     local counts = {}
-
                     -- Loop through each pixel in the chunk
-                    for j = y, y + downsizeI-1 do
-                        for i = x, x + downsizeI-1 do
-                            -- Get the color of the pixel
-                            local c = imgInput:getPixel(i, j)
 
-                            -- Increment the count for this color
-                            counts[c] = (counts[c] or 0) + 1
+                    local chunkYTrue = -1
+                    for chunkY = y, y + downsizeI-1 do
+
+                        chunkYTrue = chunkYTrue + 1
+                        local chunkXTrue = -1
+
+                        for chunkX = x, x + downsizeI-1 do
+                            chunkXTrue = chunkXTrue + 1
+
+                            -- Get the color of the pixel
+                            local color = imgInput:getPixel(chunkX, chunkY)
+
+                            --find the number to add to the count
+                            local weight = chunkYTrue+1
+                            if downsizeI-chunkYTrue < weight then weight = downsizeI-chunkYTrue end
+                            if downsizeI-chunkXTrue < weight then weight = downsizeI-chunkXTrue end
+                            if chunkXTrue+1 < weight then weight = chunkXTrue+1 end
+
+                            --apply invisible multiplyer
+                            if app.pixelColor.rgbaA(color) == 0 then 
+                                weight = weight * data.invisibleFactor
+                            end
+                            if weight ~= 0 then
+                                counts[color] = (counts[color] or 0) + weight
+                            end
                         end
                     end
 
                     -- Find the color with the highest count
                     local maxCount = 0
-                    local maxColor = nil
+                    local maxColor = app.pixelColor.rgba(0,0,0,0)
 
                     for c, count in pairs(counts) do
                         if count > maxCount then
@@ -51,7 +70,8 @@ if data.ok then
                     table.insert(colors, maxColor)
                 end
             end
-            local imgOutput = Image(imgInput.width / downsizeI, imgInput.height / downsizeI);
+            imgOutput = Image(imgInput.width / downsizeI, imgInput.height / downsizeI);
+            
 
             local iA = 1;
             for it in imgOutput:pixels() do
@@ -59,8 +79,9 @@ if data.ok then
                 iA = iA+1
             end
 
-            cel.image = imgOutput;
-        
+            cel.image = imgOutput
+            cel.position = Point(0, 0)
         end
+        spr:crop(imgOutput.bounds)
     end)
 end
